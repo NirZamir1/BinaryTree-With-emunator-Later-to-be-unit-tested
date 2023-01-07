75e1d5e13fc5e93d018b0ed771381611e7346036
nir zamir <nirozamir@gmail.com>
2023-01-07 17:59:53 +0200

    funny

--- BinaryTree With emunator/Node.cs
+++ BinaryTree With emunator/Node.cs
@@ -7,28 +7,47 @@ using System.Threading.Tasks;
 
 namespace BinaryTree_With_emunator
 {
-    public class Node<T>  : IEnumerable<T> where T : IComparable 
+    public class Node<T>  : IEnumerable<Node<T>> where T : IComparable 
     {
         T Value;
-        Node<T> parent;
+        public T GetValue { get => Value; }
+        Node<T>? parent;
         Node<T> Left;
         Node<T> Right;
         public Node(T _value)
         {
             Value = _value;
-            parent = this;
+            parent = null;
         }
-        public Node(T _value, Node<T> _parent)
+        protected Node(T _value, Node<T>? _parent )
         {
             Value= _value;
             parent= _parent;
         }
 
-        public IEnumerator<T> GetEnumerator()
+        public IEnumerator<Node<T>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new NodeEnumarator<T>(ToBottom());
         }
+        public Node<T> NextInOrder()
+        {
+            if (Right != null)
+                return Right.ToBottom();
+            if (parent != null)
+            {
+                if (parent.Left!=null && parent.Left.Equals(this))
+                {
+                    return parent;
+                }
+                else
+                {
+                    return parent.BubbleUp();
+                }
+            }
+            else
 
+                return null;
+        }
         public void Insert(T _value)
         {
             if(Value.CompareTo(_value) >= 0)
@@ -47,6 +66,17 @@ namespace BinaryTree_With_emunator
                     Right.Insert(_value);
             }
         }
+        public IEnumerable<Node<T>> Next()
+        {
+            if (Left != null)
+                Left.Next();
+
+            yield return this;
+
+            if (Right != null)
+                Right.Next();
+        }
+
         public void print()
         {
             if(Left != null)
@@ -57,8 +87,64 @@ namespace BinaryTree_With_emunator
             if(Right != null)
                 Right.print();
         }
-
+        private Node<T> ToBottom()
+        {
+            if (Left != null)
+                return Left.ToBottom();
+            else
+                return this;
+        }
+        private Node<T> BubbleUp() //Used only when in right node to be used in NextInOrder function
+        {
+            if (parent != null)
+            {
+                if (parent.Left != null && parent.Left.Equals(this))
+                    return parent;
+                else
+                    return parent.BubbleUp();
+            }
+            else
+                return null;
+        }
         IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+    
+    internal class NodeEnumarator<T> : IEnumerator<Node<T>> where T : IComparable 
+    {
+       private Node<T> _Current;
+        private int count =0;
+        public NodeEnumarator(Node<T> Node)
+        {
+            _Current = Node;
+        }
+        public Node<T> Current => _Current;
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            //hello
+        }
+        public bool MoveNext()
+        {
+            if (count == 0)
+            {
+                count++;
+                return true;
+            }
+            //throw new NotImplementedException();
+            var cur = Current.NextInOrder();
+            if (cur == null)
+                return false;
+            _Current = cur;
+            return true;
+        }
+
+        public void Reset()
         {
             throw new NotImplementedException();
         }
--- BinaryTree With emunator/Program.cs
+++ BinaryTree With emunator/Program.cs
@@ -1,8 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BinaryTree_With_emunator;
-Node<int> tree = new Node<int>(0);
+Node<double> tree = new Node<double>(0);
 tree.Insert(1);
 tree.Insert(-1);
 tree.Insert(10);
 tree.Insert(5);
+tree.Insert(6);
+tree.Insert(7);
+tree.Insert(-2);
+tree.Insert(-0.5);
 tree.print();
+Console.WriteLine("Iterator 1");
+foreach (Node<double> node in tree)
+{
+    Console.WriteLine(node.GetValue);
+}
+Console.WriteLine("Iterator 2");
+foreach (Node<double> node in tree)
+{
+    Console.WriteLine(node.GetValue);
+}
